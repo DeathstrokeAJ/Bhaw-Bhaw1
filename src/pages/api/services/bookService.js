b1e2521341ff87c897773433508b3378740c60e7
DeepaMaurya22 <deepamaurya9322@gmail.com>
2024-11-10 02:33:45 +0530

    service

--- src/pages/api/services/bookService.js
+++ src/pages/api/services/bookService.js
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
         ...formData,
         bookingID,
         userId,
+        status:'incoming',
         createdAt: new Date().toISOString(),
       });
 
